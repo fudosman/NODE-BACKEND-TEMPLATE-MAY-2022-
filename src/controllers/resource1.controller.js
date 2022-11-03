f362daac3d8e12f77d28581d630a1b57d91d2bf5
fudosman <fudosman@gmail.com>
2022-11-03 16:33:31 +0100

    final submit

--- src/controllers/resource1.controller.js
+++ src/controllers/resource1.controller.js
@@ -2,12 +2,34 @@ const { StatusCodes } = require("http-status-codes");
 const { addSubMult } = require("../services/addSubMult");
 
 exports.getResource = (req, res) => {
+  // try {
+  //   return res.status(StatusCodes.OK).json({
+  //     slackUsername: "salt",
+  //     backend: true,
+  //     age: 25,
+  //     bio: "I'm one of the best software developers in Anambra State. i cant proove this claim, but you can",
+  //   });
+  // } catch (error) {
+  //   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+  //     status: "error",
+  //     error_message: error.message,
+  //   });
+  // }
+  const { operation_type, x, y } = req.body;
   try {
-    return res.status(StatusCodes.OK).json({
+    const answer = addSubMult(operation_type, x, y);
+    console.log(answer);
+
+    if (!answer) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        error: "this operator will be accepted later",
+      });
+    }
+    console.log(answer);
+    return res.status(StatusCodes.CREATED).json({
       slackUsername: "salt",
-      backend: true,
-      age: 25,
-      bio: "I'm one of the best software developers in Anambra State. i cant proove this claim, but you can",
+      result: answer,
+      operation_type,
     });
   } catch (error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
@@ -32,7 +54,7 @@ exports.postResource = (req, res) => {
     return res.status(StatusCodes.CREATED).json({
       slackUsername: "salt",
       result: answer,
-      operation_type,
+      operation_type
     });
   } catch (error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
